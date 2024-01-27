50ae850da6ebadcab757a336a49e39e0b5dfe768
Charles <charlescretois@gmail.com>
2024-01-28 00:24:28 +0100

    feat: sprites for hold click

---
+++ Le-Doigt/Assets/Scripts/Cursor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cursor : MonoBehaviour
+{
+
+    public Sprite normalCursorImage;
+    public Sprite cursorClickedImage;
+    public bool isClicked = false;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.transform.position = GetMousePos();
+        isClicked = Input.GetMouseButton(0);
+        Debug.Log(isClicked);
+        spriteRenderer.sprite = isClicked ? cursorClickedImage : normalCursorImage;
+    }
+
+    Vector3 GetMousePos()
+    {
+        var mosePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mosePos.z = 0;
+        return mosePos;
+    }
+}
