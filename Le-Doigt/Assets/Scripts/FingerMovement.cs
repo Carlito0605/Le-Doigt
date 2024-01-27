2eff1f8acee7b66a94cf8f5ac977e2b3f76f8a89
Charles <charlescretois@gmail.com>
2024-01-27 17:01:26 +0100

    feat: success condition -> not pull too fast

--- Le-Doigt/Assets/Scripts/FingerMovement.cs
+++ Le-Doigt/Assets/Scripts/FingerMovement.cs
@@ -34,6 +34,10 @@ public class FingerMovement : MonoBehaviour
 
     public Vector3 velocity = Vector3.zero; //Vector3.zero -> Vector3(0, 0, 0)
 
+    public float speedLimit = 20f;
+
+    public bool isTooFast = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -48,27 +52,29 @@ public class FingerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        if (isSuccess()) Debug.Log("Success !");
         //horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; //Calcul notre mouvemment horizontal avec Input.GetAxis("Horizontal") récupère input (->, <-, A, D)
         Vector3 targetVelocity = new Vector2(releaseFingerSpeed * -1, rb.velocity.y); //Récupère le mouvement en 2D du joueur avec un Vector2
         if (rb.transform.position.x < rangeMin) rb.velocity = new Vector2(0, 0);
-        else if(isDragged == false) rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f); //Ramène le doigt au début
+        else if(isDragged == false) {
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f); //Ramène le doigt au début
+        }
+        updateIsTooFast();
+        Debug.Log(isTooFast);
         isDragged = false;
         MovePlayer(horizontalMovement); //Lance notre fonction Moveplayer en envoyant notre mouvement calculer au dessus
         currentMoveSpeed = GetCurrentMoveSpeed();
+       
     }
 
     public void MovePlayer(float _horizontalMovement) //Fonction qui fait bouger notre personnage
     {
-        if (horizontalMovement != 0) Debug.Log("Movement");
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y); //Récupère le mouvement en 2D du joueur avec un Vector2
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f); //Donne le mouvement au joueur
     }
 
     void OnMouseDrag()
     {
-        //Debug.Log("OnMouseDrag");
-        //transform.position = GetMousePos();
         isDragged = true;
         MovePlayer((GetMousePos().x - rb.transform.position.x) * moveSpeed);
     }
@@ -85,7 +91,17 @@ public class FingerMovement : MonoBehaviour
 
     float GetCurrentMoveSpeed()
     {
-        Debug.Log(rb.velocity.x);
         return Mathf.Abs(rb.velocity.x);
+    }
+
+    bool isSuccess()
+    {
+        return isDragged && !isTooFast && (rangeMax - rb.transform.position.x) <= 0.1f ;
+    }
+
+    void updateIsTooFast()
+    {
+        if(isDragged) isTooFast = isTooFast || currentMoveSpeed > speedLimit;
+        else isTooFast = false;
     }
 }
